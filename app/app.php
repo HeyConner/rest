6363d764ebb975ed805ad5c693cc62aed17e73b2
Ben Ronda and Conner Belvin <bronda95@comcast.net>
2016-02-25 14:42:24 -0800

    add twig files

--- app/app.php
+++ app/app.php
@@ -5,7 +5,7 @@
 
     $app = new Silex\Application();
 
-    $server = 'mysql:host=localhost;dbname=restaurant';
+    $server = 'mysql:host=localhost;dbname=restaurants';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
@@ -14,8 +14,19 @@
     Request::enableHttpMethodParameterOverride();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array (
-        'twig.path' => __DIR__.'/../views':
+        'twig.path' => __DIR__.'/../views'
     ));
+
+    $app->get("/", function() use ($app){
+        return $app['twig']->render('Index.html.twig');
+    });
+
+    $app->post("/results", function() use ($app){
+        $Restaurant = new Restaurant($_POST['restaurant'], $_POST['description']);
+        $new_restaurant = $Restaurant->save();
+
+        return $app['twig']->render('page2.html.twig', array('results' => Restaurant::getAll()));
+    });
 
 
     return $app;
